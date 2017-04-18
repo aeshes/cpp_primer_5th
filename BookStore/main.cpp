af6f0fd4f22ff20f334674ad1841834be2ea7b20
aeshes <mathesis@inbox.ru>
2017-04-18 23:13:51 +0300

    BookStore update

--- BookStore/main.cpp
+++ BookStore/main.cpp
@@ -20,8 +20,23 @@ protected:
 class bulk_quote : public quote
 {
 public:
+	bulk_quote() = default;
+	bulk_quote(const std::string &book, double p, std::size_t qty, double disc)
+		: quote(book, p), min_qty(qty), discount(disc) {}
 	double net_price(std::size_t n) const override;
+
+private:
+	std::size_t min_qty = 0;
+	double discount = 0.0;
 };
+
+double bulk_quote::net_price(std::size_t cnt) const
+{
+	if (cnt >= min_qty)
+		return cnt * (1 - discount) * price;
+	else
+		return cnt * price;
+}
 
 double print_total(std::ostream &os, const quote &item, std::size_t n)
 {
@@ -32,5 +47,9 @@ double print_total(std::ostream &os, const quote &item, std::size_t n)
 
 int main()
 {
+	quote base("xxx", 10);
+	bulk_quote derived("xxx", 10, 1, 0.1);
 
+	print_total(std::cout, base, 1);
+	print_total(std::cout, derived, 1);
 }
